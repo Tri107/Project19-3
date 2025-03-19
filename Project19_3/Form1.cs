eb82948df1e92462a84576b0908763105785c422
Tri107 <minhtri1072004@gmail.com>
2025-03-19 08:39:03 +0700

    create button and functions

--- Project19_3/Form1.cs
+++ Project19_3/Form1.cs
@@ -16,5 +16,10 @@ namespace Project19_3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test", "Warning", MessageBoxButtons.OK);
+        }
     }
 }
